11b53de4793d96f389ea19fb2a48ef051d3c36a4
Glitch (glitch-hello-website) <none>
2021-09-03 16:30:24 +0000

    📍🖐 Checkpoint ./windows.js:25912110/1814 ./index.html:25912110/50 ./drag.js:25912110/443

--- drag.js
+++ drag.js
@@ -1,12 +1,6 @@
-window.dragElement = function(elmnt) {
+window.dragElement = function(elmnt, header) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "header")) {
-    /* if present, the header is where you move the DIV from:*/
-    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-  } else {
-    /* otherwise, move the DIV from anywhere inside the DIV:*/
-    elmnt.onmousedown = dragMouseDown;
-  }
+  header.onmousedown = dragMouseDown;
 
   function dragMouseDown(e) {
     e = e || window.event;
--- windows.js
+++ windows.js
@@ -1,4 +1,20 @@
 window.createNewWindow = function(options) {
-    
-    dragElement(window, );
+    let windowElement = document.createElement("div");
+    windowElement.classList.add("window");
+    windowElement.width = options.width + "px";
+    windowElement.height = options.height + "px";
+    let title = document.createElement("h2");
+    title.style.color = "#000000";
+    title.style.width = (options.width - 40) + "px";
+    title.style.height = "32px";
+    title.style.float = "left";
+    title.innerHTML = options.title;
+    let titlebarhitbox = document.createElement("div");
+    // position:absolute;top:0;left:0;width:calc(100% - 40px);height:32px;
+    titlebarhitbox.style.width = (options.width - 40) + "px";
+    titlebarhitbox.style.height = "32px";
+    document.getElementById("desktop").appendChild(windowElement);
+    windowElement.appendChild(title);
+    window.dragElement(windowElement, titlebarhitbox);
 }
+createNewWindow({width:400,height:200, title: "Windows"});
