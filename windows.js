7a6be6f1a54607a73eb1ed1864557c737c7b6c9e
Glitch (glitch-hello-website) <none>
2021-09-04 12:02:50 +0000

    👨‍❤️‍👨🚐 Checkpoint ./menu.css:25912110/77 ./windows.js:25912110/11976 ./index.html:25912110/29

--- windows.js
+++ windows.js
@@ -1,12 +1,14 @@
-import interact from 
-'https://cdn.interactjs.io/v1.10.11/interactjs/index.js'
+import interact from 'https://cdn.interactjs.io/v1.10.11/interactjs/index.js'
 
 let windowPos = 0;
 window.createNewWindow = function(options) {
     function exit() {
-      windowElement.style.opacity=0;
-      windowElement.style.transform='scale(0.9)';
-      setTimeout(function(){windowElement.remove()},200);
+        windowElement.style.opacity = 0;
+        windowElement.style.transform = 'scale(0.9)';
+        windowElement.style.transition = "opacity .2s, transform .2s";
+        setTimeout(function() {
+            windowElement.remove()
+        }, 200);
     }
     windowPos++;
     if (windowPos == 8) windowPos = 0;
@@ -35,22 +37,22 @@ window.createNewWindow = function(options) {
     let closeIcon = document.createElement("div");
     closeIcon.classList.add("close");
     close.onclick = function() {
-      exit();
-      if (end) end();
+        exit();
+        if (end) end();
     };
     close.onmouseover = function() {
-      closeIcon.style.backgroundColor = "#ffffff";
+        closeIcon.style.backgroundColor = "#ffffff";
     };
     close.onmouseout = function() {
-      closeIcon.style.backgroundColor = "#000000";
+        closeIcon.style.backgroundColor = "#000000";
     };
     let content = document.createElement("div");
     content.style.position = "absolute";
     content.style.top = "32px";
     content.style.left = "1px";
     content.style.backgroundColor = "#f4f6f9";
-    content.style.width = (options.width - 2) + "px";
-    content.style.height = (options.height - 33) + "px";
+    content.style.width = "calc(100% - 2px)" //(options.width - 2) + "px";
+    content.style.height = "calc(100% - 33px)" //(options.height - 33) + "px";
     content.style.borderBottomRightRadius = "5px";
     content.style.borderBottomLeftRadius = "5px";
     windowElement.appendChild(title);
@@ -58,28 +60,49 @@ window.createNewWindow = function(options) {
     windowElement.appendChild(titlebarhitbox);
     windowElement.appendChild(content);
     close.appendChild(closeIcon);
-    //window.dragElement(windowElement, titlebarhitbox);
-interact(windowElement)
-  .resizable({
-    edges: { top: true, left: true, bottom: true, right: true },
-    listeners: {
-      move: function (event) {
-        let { x, y } = event.target.dataset
+    interact(windowElement)
+        .resizable({
+            edges: {
+                top: true,
+                left: true,
+                bottom: true,
+                right: true
+            },
+            listeners: {
+                move: function(event) {
+                    let {
+                        x,
+                        y
+                    } = event.target.dataset
 
-        x = (parseFloat(x) || 0) + event.deltaRect.left
-        y = (parseFloat(y) || 0) + event.deltaRect.top
+                    x = (parseFloat(x) || 0) + event.deltaRect.left
+                    y = (parseFloat(y) || 0) + event.deltaRect.top
 
-        Object.assign(event.target.style, {
-          width: `${event.rect.width}px`,
-          height: `${event.rect.height}px`,
-          transform: `translate(${x}px, ${y}px)`
+                    Object.assign(event.target.style, {
+                        width: `${event.rect.width}px`,
+                        height: `${event.rect.height}px`,
+                        transform: `translate(${x}px, ${y}px)`
+                    })
+
+                    Object.assign(event.target.dataset, {
+                        x,
+                        y
+                    })
+                }
+            },
+            modifiers: [
+
+                // minimum size
+                interact.modifiers.restrictSize({
+                    min: {
+                        width: 200,
+                        height: 100
+                    }
+                })
+            ],
         })
-
-        Object.assign(event.target.dataset, { x, y })
-      }
-    }
-  })
+    window.dragElement(windowElement, titlebarhitbox);
     if (options.script) {
-      end = options.script(windowElement, title, content, exit);
+        end = options.script(windowElement, title, content, exit);
     };
 }
