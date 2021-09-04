4ac2ded7e6b9758539b122e9f0ffc6f761808235
Glitch (glitch-hello-website) <none>
2021-09-04 11:51:34 +0000

    🖐🏝 Checkpoint ./windows.js:25912110/2499 ./menu.css:25912110/192

--- windows.js
+++ windows.js
@@ -58,7 +58,27 @@ window.createNewWindow = function(options) {
     windowElement.appendChild(titlebarhitbox);
     windowElement.appendChild(content);
     close.appendChild(closeIcon);
-    window.dragElement(windowElement, titlebarhitbox);
+    //window.dragElement(windowElement, titlebarhitbox);
+interact(windowElement)
+  .resizable({
+    edges: { top: true, left: true, bottom: true, right: true },
+    listeners: {
+      move: function (event) {
+        let { x, y } = event.target.dataset
+
+        x = (parseFloat(x) || 0) + event.deltaRect.left
+        y = (parseFloat(y) || 0) + event.deltaRect.top
+
+        Object.assign(event.target.style, {
+          width: `${event.rect.width}px`,
+          height: `${event.rect.height}px`,
+          transform: `translate(${x}px, ${y}px)`
+        })
+
+        Object.assign(event.target.dataset, { x, y })
+      }
+    }
+  })
     if (options.script) {
       end = options.script(windowElement, title, content, exit);
     };
