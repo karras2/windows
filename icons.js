e10432d0a59050c0b3911ba248d98b3a0dba63eb
Glitch (glitch-hello-website) <none>
2021-09-04 11:28:53 +0000

    🐲💾 Checkpoint ./index.html:25912110/45 ./menu.css:25912110/16

--- icons.js
+++
@@ -1,9 +0,0 @@
-function getIconFile(name, size) {
-  let icon = new Image();
-  icon.src = "./icons/" + name + size + "x" + size + ".png";
-  return icon;
-};
-// 0: MSStore
-window.icons = [
-  [getIconFile(name, 32)]
-];
